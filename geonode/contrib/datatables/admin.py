a9fb88e0f276e5d7805172ecbb18e36bc9c117f3
Jeffrey Johnson <ortelius@gmail.com>
2015-02-12 12:59:54 -0800

    update DataTableAdmin

--- geonode/contrib/datatables/admin.py
+++ geonode/contrib/datatables/admin.py
@@ -3,6 +3,13 @@ from .models import DataTable, TableJoin, JoinTarget
 
 class DataTableAdmin(admin.ModelAdmin):
     model = DataTable
+    list_display = (
+        'id',
+        'title',
+        'table_name',
+        'tablespace')
+    list_display_links = ('title',)
+
 
 class TableJoinAdmin(admin.ModelAdmin):
     model = TableJoin 
