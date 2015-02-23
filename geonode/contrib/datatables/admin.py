380158c3d5d8840700dfbef28ead8ce9e37d0e23
Jeffrey Johnson <ortelius@gmail.com>
2015-02-23 12:00:58 -0800

    Allow for filtering of the attributes in the JoinTargetAdmin

--- geonode/contrib/datatables/admin.py
+++ geonode/contrib/datatables/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from geonode.layers.models import Attribute
 from .models import DataTable, TableJoin, JoinTarget, JoinTargetFormatType, GeocodeType
 
 class DataTableAdmin(admin.ModelAdmin):
@@ -16,6 +17,13 @@ class TableJoinAdmin(admin.ModelAdmin):
 
 class JoinTargetAdmin(admin.ModelAdmin):
     model = JoinTarget
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'attribute':
+            kwargs["queryset"] = Attribute.objects.filter(
+                resource=request.GET.get('layer'))
+        return super(JoinTargetAdmin, self).formfield_for_foreignkey(
+            db_field, request, **kwargs)
 
 admin.site.register(DataTable, DataTableAdmin)
 admin.site.register(TableJoin, TableJoinAdmin)
