414a0a93e6c15cb511d1aaff066bbe2be96ba57d
karakostis <karakostis.dimitris@gmail.com>
2016-06-03 11:51:50 +0200

    changes in settings and url.py file for the datatables contrib app

--- geonode/settings.py
+++ geonode/settings.py
@@ -242,6 +242,7 @@ GEONODE_APPS = (
 GEONODE_CONTRIB_APPS = (
     # GeoNode Contrib Apps
     'geonode.contrib.dynamic',
+    'geonode.contrib.datatables',
     'geonode.contrib.exif',
     'geonode.contrib.favorite',
     'geonode.contrib.geogig',
